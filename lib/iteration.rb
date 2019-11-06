db44dfe28f47464d351bdc0c964c455673cf454b
Madeline Corman <madelinecorman@gmail.com>
2019-11-06 16:43:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,15 +13,7 @@ def join_ingredients(src)
     ["onions", "pineapple"]
     ]
     
-    row_index = 0 
-    while row_index < array_1.count do 
-      element_index = 0 
-      while element_index < array_1[row_index].count do
-        puts "I love ${array_1[row_index][0]} and ${array_1[row_index][1]} on my pizza"
-      end
-      row_index += 1 
-      
-    end
+    
     
 end
 
